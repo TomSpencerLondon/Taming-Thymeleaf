cebe3e49daa4101d944071c1ae61da48a72c7d41
Tom Spencer <thomas.spencer@codurance.com>
2022-11-19 10:18:13 +0000

    Add template with references to model from controller

---
+++ src/main/java/com/tamingthymeleaf/tamingthymeleaf/RootController.java
@@ -0,0 +1,18 @@
+package com.tamingthymeleaf.tamingthymeleaf;
+
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class RootController {
+  @GetMapping public String index(Model model) {
+    model.addAttribute("pageTitle", "Taming Thymeleaf");
+    model.addAttribute("scientists", List.of("Albert Einstein", "Niels Bohr", "James Clerk Maxwell"));
+    return "index";
+
+  }
+}
